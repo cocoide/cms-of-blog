5b0420329b1af315c70f5e263d3ef6f5ecd0d327
{cocoide} <{cocoide.com@gmail.com}>
2022-10-16 19:09:31 +0900

    set layout with header and footer

---
+++ components/header.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header className="border-b flex items-center h-14 px-4">
+      Header
+    </header>
+  )
+}
+export default Header;
---
+++ components/layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import Footer from "./footer";
+import Header from "./header";
+
+type Props = { children: ReactNode };
+
+
+const Layout = ({ children }: Props) => {
+  return (
+  <div>
+    <Header/>
+    <main>{children}</main>
+    <Footer/>
+    </div>
+    );
+};
+
+export default Layout;
